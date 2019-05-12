cd5788358e863bb57aadc5906ef647cd81ebf64f
Aashish Sharma <aashish.sharma.108@gmail.com>
2019-05-12 19:34:57 +0100

    Tidied up some code for FB API client-side.

--- public/js/facebookApi.js
+++ public/js/facebookApi.js
@@ -11,55 +11,13 @@ window.fbAsyncInit = function() {
       console.log('Welcome!  Fetching your information.... ');
       FB.api('/me', function(response) {
         console.log('Good to see you, ' + response.name + '.');
-
-        // FB.api('me/accounts', 'get', {}, function(response) {
-        //   if (!response || response.error) {
-        //     console.log(response.error);
-        //   } else {
-        //     console.log(response);
-        //   }
-        // });
-
-        // FB.api('me?fields=id,name', 'get', {}, function(response) {
-        //   if (!response || response.error) {
-        //     console.log(response.error);
-        //   } else {
-        //     console.log(response);
-        //   }
-        // });
-
-        // FB.api('161715481383/albums', 'get', {}, function(response) {
-        //   if (!response || response.error) {
-        //     console.log(response.error);
-        //   } else {
-        //     console.log(response);
-        //   }
-        // });
-
         FB.api('161715481383?fields=access_token', 'get', {}, function(response) {
           if (!response || response.error) {
             console.log(response.error);
           } else {
             console.log(response);
             const token = response.access_token;
-            document.getElementById("fbToken").value = token; 
-            // FB.api(
-            //   "/161715481383/feed",
-            //   "POST",
-            //   {
-            //       "published": "false",
-            //       // "child_attachments": "[\n    {\n      link: \"https://yahoo.com\",\n      name: \"name -1\",\n      picture: \"https://images.all-free-download.com/images/graphiclarge/ethereal_image_185195.jpg\"\n    },\n    {\n      link: \"https://google.com\",\n      name: \"name - 2 \",\n      picture: \"https://images.all-free-download.com/images/graphiclarge/ethereal_image_185195.jpg\"\n    }\n  ]",
-            //       "message": "test",
-            //   },
-            //   function (response) {
-            //     if (!response || response.error) {
-            //       console.log(response.error);
-            //     } else {
-            //       console.log(response);
-            //     }
-            //   }
-            // );
-
+            document.getElementById("fbToken").value = token;
           }
         });
 
@@ -73,5 +31,3 @@ window.fbAsyncInit = function() {
       enable_profile_selector: true,
     });
 };
-
-fbAsyncInit();
