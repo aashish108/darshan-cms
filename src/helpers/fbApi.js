7334a88b09471c08e19d4d76a92a1dffd37c4143
Sovereign108 <aashish.sharma.108@gmail.com>
2019-12-31 17:59:02 +0000

    Deleted test FB code and added host to .env.

--- src/helpers/fbApi.js
+++ src/helpers/fbApi.js
@@ -15,8 +15,7 @@ class FacebookApi {
   async urlConstruction() {
     const parts = [];
     for (const file of this.darshan[0].files) {
-      // parts.push(`{link:'https://localhost/${file.destination}/${file.filename}'},`);
-      parts.push("{link:'https://images.all-free-download.com/images/graphiclarge/ethereal_image_185195.jpg'},");
+      parts.push(`{link:'${process.env.HOST}/${file.destination}/${file.filename}'},`);
     }
     const childAttachments = encodeURI(`[${parts.join('')}]`);
     const url = `https://graph.facebook.com/v3.3/161715481383/feed?message=test12345&child_attachments=${childAttachments}&link=https://www.iskcon.london&access_token=${this.fbPageToken}`;
@@ -37,15 +36,11 @@ class FacebookApi {
       this.res.end();
     } catch (e) {
       console.log(e);
-    } 
+    }
   }
 
   static callback(error, response, body) {
-    console.log('Ín callback!');
-    // console.log(response);
-    console.log(body);
     if (!error && response.statusCode === 200) {
-      // window.location.href = 'http://localhost:3000/node/darshan-app/stage3/facebook/upload/confirmation';
       console.log(body);
     } else if (error) {
       console.log(error);
