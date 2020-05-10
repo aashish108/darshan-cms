d4a5847a748e5abe91a2159b43ff5bc4fbc7f896
Aashish Sharma <aashish.sharma.108@gmail.com>
2020-05-10 12:24:52 +0100

    Tidying up FB API code

--- public/js/facebookApi.js
+++ public/js/facebookApi.js
@@ -1,33 +1,34 @@
-window.fbAsyncInit = function() {
+window.fbAsyncInit = () => {
   FB.init({
-    appId            : '2316161761778187',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v3.3'
+    appId: '2316161761778187',
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: 'v3.3',
   });
 
-  FB.login(function(response) {
+  FB.login((response) => {
     if (response.authResponse) {
       console.log('Welcome!  Fetching your information.... ');
-      FB.api('/me', function(response) {
-        console.log('Good to see you, ' + response.name + '.');
-        FB.api('161715481383?fields=access_token', 'get', {}, function(response) {
+
+      FB.api('/me', () => {
+        console.log(`Good to see you, ${response.name}.`);
+
+        FB.api('161715481383?fields=access_token', 'get', {}, () => {
           if (!response || response.error) {
             console.log(response.error);
           } else {
             console.log(response);
             const token = response.access_token;
-            document.getElementById("fbToken").value = token;
+            document.getElementById('fbToken').value = token;
           }
         });
-
       });
     } else {
-    console.log('User cancelled login or did not fully authorize.');
+      console.log('User cancelled login or did not fully authorize.');
     }
   }, {
-      scope: 'manage_pages,pages_show_list,publish_pages',
-      return_scopes: true,
-      enable_profile_selector: true,
-    });
+    scope: 'manage_pages,pages_show_list,publish_pages',
+    return_scopes: true,
+    enable_profile_selector: true,
+  });
 };
