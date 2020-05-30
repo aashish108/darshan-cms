b3dbe2a4fc3805c77995686e9c1a9da7f10c52ca
Aashish Sharma <aashish.sharma.108@gmail.com>
2020-05-30 20:05:08 +0100

    Got FB API POST working

--- src/frontend/js/_modules/Facebook.js
+++ src/frontend/js/_modules/Facebook.js
@@ -4,6 +4,7 @@ class Facebook {
     this.appId = '2316161761778187';
     this.fbAppVersion = 'v7.0';
     this.fbPageId = '161715481383';
+    this.userId = '10157276554802300';
     const fbScopesArray = [
       'pages_manage_posts',
       'pages_show_list',
@@ -26,63 +27,72 @@ class Facebook {
     FB.init({
       appId: this.appId,
       autoLogAppEvents: true,
-      xfbml: true,
+      xfbml: false,
       version: this.fbAppVersion,
+      status: true,
     });
   }
 
   uploadToFacebookButton() {
     const button = document.querySelector('form button');
-    const self = this;
     button.addEventListener('click', (event) => {
       event.preventDefault();
-      self.login();
+      this.login();
     });
   }
 
   login() {
-    const self = this;
     FB.login((response) => {
       if (response.authResponse) {
         console.log('Welcome!  Fetching your information.... ');
-
-        FB.api('/me', () => {
-          console.log(`Good to see you, ${response.name}.`);
-
-          FB.api(`${this.fbPageId}?fields=access_token`, 'get', {}, () => {
-            if (!response || response.error) {
-              console.log(response.error);
-            } else {
-              console.log(response);
-              const token = response.authResponse.accessToken;
-              document.getElementById('fbToken').value = token;
-              this.post(self);
-            }
-          });
-        });
+        console.log('inital user login FB response', response.authResponse);
+        this.getPageTokenFromUserToken();
       } else {
         console.log('User cancelled login or did not fully authorize.');
       }
     }, {
-      scope: this.fbScope,
+      scope: this.fbScopes,
       return_scopes: true,
       enable_profile_selector: true,
     });
   }
 
-  post() {
+  getPageTokenFromUserToken() {
+    FB.api('/me', () => {
+      console.log('Good to see you.');
+      FB.api(`${this.userId}/accounts`, 'get', {}, (userAccountsResponse) => {
+        console.log('userAccountsResponse', userAccountsResponse);
+        const pageToken = userAccountsResponse.data.map((account) => {
+          console.log(`FB account ${account.name} found.`);
+          console.log('this.fbPageId', this.fbPageId);
+          console.log('account.id', account.id);
+          if (account.id === this.fbPageId) {
+            return account.access_token;
+          }
+        });
+        console.log(`FB Page account token is ${pageToken}`);
+        document.getElementById('fbToken').value = pageToken;
+        this.post(pageToken);
+      });
+    });
+  }
+
+  post(pageToken) {
     console.log('this.latestDarshanImages[0].outfitDetails', this.latestDarshanImages[0].outfitDetails);
     FB.api(
-      `/${this.fbPageId}/feed`,
+      `/${this.fbPageId}/feed?access_token=${pageToken}`,
       'POST',
       {
         message: this.latestDarshanImages[0].outfitDetails,
         child_attachments: this.childAttachments(),
         link: this.darshanUrl,
+        multi_share_optimized: true,
       },
       (fbResponse) => {
         if (fbResponse && !fbResponse.error) {
           window.location.href = `${this.baseAppUrl}/darshan-app/stage3/facebook/upload/confirmation`;
+        } else if (fbResponse.error) {
+          console.log('FB error: ', fbResponse.error);
         }
       },
     );
